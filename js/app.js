ba0d492696f118c52f6866718ef6b1d7b5ffd37b
Brent Woodward <Brent.Woodward.1@gmail.com>
2018-10-10 18:08:11 -0700

    added q7, renamed variables in q6 & q7

--- js/app.js
+++ js/app.js
@@ -1,63 +1,63 @@
 'use strict';
 
-// //collect user data and greeting message
-// alert('Welcome! Let\'s play a game to see how well you know me!');
+//collect user data and greeting message
+alert('Welcome! Let\'s play a game to see how well you know me!');
 
 var username = prompt('First of all, what is your name?');
 console.log('User is registered as ' + username + '.');
 
-// alert('Excellent! Welcome, ' + username + ' . Let\'s do this.');
+alert('Excellent! Welcome, ' + username + ' . Let\'s do this.');
 
 // //questions 1-5
-// var answerOne = prompt('Have I broken a bone before?').toLowerCase();
-// console.log(answerOne)
-// if (answerOne === 'yes' || answerOne === 'y') {
-//   alert('You are correct! It was one of my pinky finger bones on the right hand. Commonly known as a Boxer\'s Fracture.');
-//   console.log(username + ' answered the first question correctly!');
-// } else {
-//   alert('You\'re incorrect. I have broken one bone.');
-//   console.log(username + ' answered the first question incorrectly.');
-// }
+var answerOne = prompt('Have I broken a bone before?').toLowerCase();
+console.log(answerOne)
+if (answerOne === 'yes' || answerOne === 'y') {
+  alert('You are correct! It was one of my pinky finger bones on the right hand. Commonly known as a Boxer\'s Fracture.');
+  console.log(username + ' answered the first question correctly!');
+} else {
+  alert('You\'re incorrect. I have broken one bone.');
+  console.log(username + ' answered the first question incorrectly.');
+}
 
-// var answerTwo = prompt('Is Halloween my favorite holiday?').toLowerCase();
-// console.log(answerTwo);
-// if (answerTwo === 'no' || answerTwo === 'n') {
-//   alert(username + ' you are correct! Most people don\'t know that about me.');
-//   console.log(username + ' answered the second questions correctly!');
-// } else {
-//   alert(username + ', it\'s like you have never met me!');
-//   console.log(username + ' has answered the second question incorrectly.');
-// }
+var answerTwo = prompt('Is Halloween my favorite holiday?').toLowerCase();
+console.log(answerTwo);
+if (answerTwo === 'no' || answerTwo === 'n') {
+  alert(username + ' you are correct! Most people don\'t know that about me.');
+  console.log(username + ' answered the second questions correctly!');
+} else {
+  alert(username + ', it\'s like you have never met me!');
+  console.log(username + ' has answered the second question incorrectly.');
+}
 
-// var answerThree = prompt('Do I live in Seattle, WA?').toLowerCase();
-// console.log(answerThree);
-// if (answerThree === 'no' || answerThree === 'n') {
-//   alert('Very good! I live in Bothell, WA.');
-//   console.log(username + ' has gotten the third question correct.');
-// } else {
-//   alert('I am sorry ' + username + '. I actually live in Bothell, WA.');
-//   console.log(username + ' has answered the third question incorrectly.');
-// }
+var answerThree = prompt('Do I live in Seattle, WA?').toLowerCase();
+console.log(answerThree);
+if (answerThree === 'no' || answerThree === 'n') {
+  alert('Very good! I live in Bothell, WA.');
+  console.log(username + ' has gotten the third question correct.');
+} else {
+  alert('I am sorry ' + username + '. I actually live in Bothell, WA.');
+  console.log(username + ' has answered the third question incorrectly.');
+}
 
-// var answerFour = prompt('Am I an avid tobacco pipe and cigar smoker?').toLowerCase();
-// console.log(answerFour);
-// if (answerFour === 'yes' || answerFour === 'y') {
-//   alert('You\'re darn toot\'n! I will take a nice Latakia in my pipe or a wonderful Arturo Fuente Rosado Sungrown cigar any day of the week!');
-//   console.log(username + ' has gotten the fourth question correct.');
-// } else {
-//   alert('Wow, ' + username + '! We can\'t be friends any longer.');
-//   console.log(username + ' has answered the fourth question incorrectly.');
-// }
+var answerFour = prompt('Am I an avid tobacco pipe and cigar smoker?').toLowerCase();
+console.log(answerFour);
+if (answerFour === 'yes' || answerFour === 'y') {
+  alert('You\'re darn toot\'n! I will take a nice Latakia in my pipe or a wonderful Arturo Fuente Rosado Sungrown cigar any day of the week!');
+  console.log(username + ' has gotten the fourth question correct.');
+} else {
+  alert('Wow, ' + username + '! We can\'t be friends any longer.');
+  console.log(username + ' has answered the fourth question incorrectly.');
+}
 
-// var answerFive = prompt('Do I like board games?').toLowerCase();
-// console.log(answerFive);
-// if (answerFive === 'yes' || answerFive === 'y') {
-//   alert(username + ', that is right. Let me know if you ever want to play!');
-//   console.log(username + ' has gotten the fifth and final question correct.');
-// } else {
-//   alert('You couldn\'t be more wrong.');
-//   console.log(username + ' has answered the fifth question correctly.');
-// }
+var answerFive = prompt('Do I like board games?').toLowerCase();
+console.log(answerFive);
+if (answerFive === 'yes' || answerFive === 'y') {
+  alert(username + ', that is right. Let me know if you ever want to play!');
+  console.log(username + ' has gotten the fifth and final question correct.');
+} else {
+  alert('You couldn\'t be more wrong.');
+  console.log(username + ' has answered the fifth question correctly.');
+}
 
 // Variables are established and the user is prompted for a guess. The attempt is incremented for every incorrect guess. Terminates when correct answer given.
 var questionSixGuessLimit = 4;
@@ -67,7 +67,6 @@ var myCountriesVisited = 3;
 while (questionSixAttempt < questionSixGuessLimit) {
   var answerSix = Number(prompt('Guess how many countries I have been to!'));
   questionSixAttempt++;
-
   if (answerSix === myCountriesVisited) {
     alert(username + ', ' + answerSix + ' is correct. I have been to Jamaica, Canada, and the US! It took you ' + questionSixAttempt + ' attempt(s).');
     console.log(username + ' has answered question 6 correctly with an input of: ' + answerSix);
@@ -83,5 +82,25 @@ while (questionSixAttempt < questionSixGuessLimit) {
   if (questionSixAttempt === questionSixGuessLimit) {
     alert('I am sorry ' + username + '. You only had ' + questionSixGuessLimit + ' chances to guess.');
     console.log(username + ' has failed to guess in ' + questionSixGuessLimit + ' attempts.')
+  }
+}
+
+var questionSevenGuessLimit = 6;
+var questionSevenAttempt = 0;
+var remainingAttempts = questionSevenGuessLimit - 1;
+var myLarps = ['alliance', 'spite', 'camarilla', 'sabbat', 'werewolf'];
+while (questionSevenAttempt < questionSevenGuessLimit) {
+  var answerSeven = prompt(username + ' can you name a Live Action Role Play that I have participated in?').toLowerCase();
+  questionSevenAttempt++;
+
+  if (myLarps.includes(answerSeven)) {
+    alert('Absolutely! I always have such a good time at ' + answerSeven + '.')
+    alert('Here is a list of all of the LARPs that I have played in: ' + myLarps.toString());
+    break;
+  } else {
+    alert('I haven\'t tried ' + answerSeven + ' yet. I will add it to my todo list. Try again! I will give you ' + remainingAttempts-- + ' chance(s).');
+  }
+  if (questionSevenAttempt === questionSevenGuessLimit) {
+    alert('I am sorry ' + username + ' you only had ' + questionSevenGuessLimit + ' chances. I have played in ' + myLarps.toString());
   }
 }
